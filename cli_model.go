40e3d241a2b1f68f23f0aabce634622ba9b27373
Tanvir Rahman <tanvir3080@gmail.com>
2018-09-04 19:18:26 +0600

    All model seperated in another file and website ls command added

---
+++ cli_model.go
@@ -0,0 +1,51 @@
+package main
+
+type User struct {
+	Id       string `json:"id"`
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Distribution struct {
+	Id     string  `json:"id"`
+	Origin string  `json:"origin"`
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Usage  float64 `json:"usage"`
+	Status string  `json:"status"`
+}
+
+type DistributionList []Distribution
+
+type TokenSettings struct {
+	Token string
+}
+
+type Response struct {
+	Data  map[string]interface{}
+	Error map[string]interface{}
+}
+
+type ErrorResponse struct {
+	Status int `json:"status"`
+	Error  struct {
+		Code        int    `json:"code"`
+		Description string `json:"description"`
+		Parameter   string `json:"parameter"`
+	} `json:"error"`
+	Data struct {
+	} `json:"data"`
+}
+
+type WebsiteResponse struct {
+	Status int `json:"status"`
+	Error  struct {
+	} `json:"error"`
+	Data []struct {
+		WebsiteURL  string      `json:"website_url"`
+		WebsiteType string      `json:"website_type"`
+		WebsiteName interface{} `json:"website_name"`
+		ID          string      `json:"id"`
+	} `json:"data"`
+}
